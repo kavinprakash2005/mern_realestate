f677143bd7626fac083520e371bc80c9eff42798
kavinprakash2005 <kavinprakashmp12@gmail.com>
2025-02-14 19:32:06 +0530

    create api and routes and problems occures and mongodb fix it

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from '../models/user.modal.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup =async (req,res) => {
+  const { username, email,password } = req.body;
+  const hashedpassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User ({ username,email,password:hashedpassword });
+  try{
+    await newUser.save();
+    res.status(201).json( 'user create successfull!');
+  }
+ catch(error){
+  res.status(500).json(error.message);
+ }
+
+};
